d387d49f493bb06201fedd90b23eb9ba5743bcca
John Ketiku <144270920+John-Ketiku-1@users.noreply.github.com>
2024-09-18 20:55:25 -0400

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,157 +1,35 @@
 #!/bin/env bash
 # Created by: John
 # Created on: Sept 2024
+# Pi Zero 2 W .dotfiles and setup script
 
-# you will need to change permissions on the file before you can run it
-# chmod +x ./load.sh
+sudo apt update
+sudo apt upgrade --without-new-pkgs
+sudo apt full-upgrade
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
 
-# then at the end, reboot for everything to take effect
-# sudo reboot now
+# common programs
+sudo apt install git -y
+sudo apt install gh -y
+sudo apt install stow -y
+sudo apt install neofetch -y
 
+# install NeoVim, new version, now part of Rasbian :)
 
-
-# update and upgrade system
-echo Update and upgrade system
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-
-# load some programs
-echo Load programs
-sudo apt install tree
-sudo apt install unzip
-sudo apt-get -y install python3-pip
-
-# load .vimrc file
-echo Load .vimrc file
-cp ./.vimrc ~/.vimrc
-
-# load .bashrc file
-echo Load .bashrc file
-> ~/.bashrc
-cp ./.bashrc ~/.bashrc
-# dircolors -b >> .bashrc
-sudo chown ubuntu ~/.bash_profile
-echo '' >> ~/.bash_profile
-echo 'if [ -f $HOME/.bashrc ]; then' >> ~/.bash_profile
-echo '    source $HOME/.bashrc' >> ~/.bash_profile
-echo 'fi' >> ~/.bash_profile
-
-
-# load .gitconfig file
-echo Load .gitconfig file
-cp ./.gitconfig ~/.gitconfig
-
-# copy over shell script file
-echo Load shell script files
-mkdir ~/scripts
-cp ./repo.sh ~/scripts/repo.sh
-sudo chmod +x ~/scripts/repo.sh
-cp ./git-push.sh ~/scripts/git-push.sh
-sudo chmod +x ~/scripts/git-push.sh
-cp ./java-lint.sh ~/scripts/java-lint.sh
-sudo chmod +x ~/scripts/java-lint.sh
-cp ./main.yml ~/scripts/main.yml
-cp ./swift.yml ~/scripts/swift.yml
-
-# load YouCompleteMe
-echo Load YouCompleteMe plugin for Vim
-# need to do an upgrade to python libraries first
-sudo pip3 install --upgrade requests
-sudo apt install vim-youcompleteme -y
-vim-addon-manager install youcompleteme
-
-# load java programming software
-echo load Java
-sudo apt install default-jdk -y
-
-# loading checkstyle for java
-# https://github.com/checkstyle/checkstyle/releases
-echo load CheckStyle for Java
-wget https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.44/checkstyle-8.44-all.jar
-cp ./checkstyle-8.44-all.jar ~/scripts/checkstyle.jar
-wget https://raw.githubusercontent.com/Mr-Coxall/dot_files/main/mr-coxall_checks.xml
-cp ./mr-coxall_checks.xml ~/scripts/
-
-
-# you might need to get a newer version of swift
-# https://swift.org/download/
-echo load Swift
-sudo apt-get install -y clang libblocksruntime0 libcurl4-openssl-dev -y
-wget https://swift.org/builds/swift-5.5-release/ubuntu2004/swift-5.5-RELEASE/swift-5.5-RELEASE-ubuntu20.04.tar.gz
-tar -zxvf swift-5.5-RELEASE-ubuntu20.04.tar.gz
-sudo mkdir /usr/bin/swift
-sudo cp -R ./swift-5.5-RELEASE-ubuntu20.04/usr/* /usr/bin/swift
-echo "" >> ~/.bashrc
-echo 'export PATH="${PATH}":/usr/bin/swift/bin' >> ~/.bashrc
-
-# SwiftLint
-# https://github.com/realm/SwiftLint/releases to get latest release
-echo load SwiftLint for Swift
-wget https://github.com/realm/SwiftLint/releases/download/0.44.0/swiftlint_linux.zip
-unzip -n swiftlint_linux.zip
-sudo mkdir /usr/bin/swiftlint
-sudo cp ./swiftlint /usr/bin/swiftlint/
-echo 'export PATH="${PATH}":/usr/bin/swiftlint' >> ~/.bashrc
-echo "" >> ~/.bashrc
-
-# Swift syntax highlighting for Vim
-# Original Source: http://wingsquare.com/blog/swift-script-syntax-highlighting-and-indentation-for-vim-text-editor/
-# Another helpful article: https://billyto.github.io/blog/swift-syntax-vim
-# More about Vim packages: http://vimcasts.org/episodes/packages/
-echo "--- creating ~/.vim/pack/bundle/start dir.."
-mkdir -p ~/.vim/pack/bundle/start
-echo "--- Cloning Apple's Swift repo.."
-git clone --depth=1 https://github.com/apple/swift/
-echo "--- Copying plugin to Vim bundles.."
-cp -r ./swift/utils/vim ~/.vim/pack/bundle/start/swift
-# echo "--- Cleaning up, removing swift repo.."
-# rm -rf ./swift/
-
-# load GitHub CLI
-echo load GitHub CLI
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt install gh
-
-
-# reboot
-echo ---
-echo rebooting now ...
-echo ---
-sudo reboot now
-
-
-# after reboot, need to run the following by hand
-
-# provision GitHub
-ssh-keygen -t ed25519 -C "mr.coxall@mths.ca"
-eval "$(ssh-agent -s)"
-# then copy public key over to GitHub SSH keys
-cat ~/.ssh/id_ ... .pub
-# to test it out
-ssh -T git@github.com
-
-# configure GitHub CLI
-gh auth login
-# web method most likely the easiest
-# use existing GitHub SSH keys
-
-# install NeoVim from source
-mkdir temp
-cd temp || exit
-sudo apt-get install ninja-build gettext cmake unzip curl -y
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-cd .. || exit
-cd .. || exit
-sudo rm -r ./temp
-
-# install languages, bun and Java
-sudo apt install -y default-jdk
+# install languages
 curl -fsSL https://bun.sh/install | bash
-# shellcheck source=/dev/null
-source ~/.bashrc
+sudo apt install -y default-jdk
 
-# then remove the dot_files firectory 
-sudo rm -R ~/dot_files
+# install Fish Shell
+sudo apt install gpg -y
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+sudo apt update
+sudo apt install fish -y
+
+# install Starship for Fish
+curl -sS https://starship.rs/install.sh | sh
+
+sudo apt remove w3m -y
