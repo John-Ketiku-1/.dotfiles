2d1dc0c53447fe7417f1cb14acebe7002b647567
John Ketiku <144270920+John-Ketiku-1@users.noreply.github.com>
2024-10-03 09:06:34 -0400

    added new nvim designs

--- .config/nvim/lua/plugins/Isp-config.lua
+++ .config/nvim/lua/plugins/Isp-config.lua
@@ -1,36 +1,33 @@
--- you will have to ensure npm and typescript are installed globally
---
--- sudo apt install nodejs
--- sudo apt install npm
--- sudo npm i -g typescript-language-server; sudo npm i -g typescript
+-- you need to have NPM loaded for the TS LSP server, so ensure it it installed
+-- sudo apt install npm -y
 
 return {
-    {
-      "williamboman/mason.nvim",
-      lazy = false,
-      config = function()
-        require("mason").setup()
-      end
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      lazy = false,
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {"lua_ls", "tsserver", "jdtls"},
-        })
-      end
-    },
-    {
-      "neovim/nvim-lspconfig",
-      lazy = false,
-      config = function()
-        local lspconfig = require("lspconfig")
-        
-        lspconfig.lua_ls.setup({})
-        lspconfig.tsserver.setup({})
-        lspconfig.jdtls.setup({})
-      end
-    }
+  {
+    "williambom:qan/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "bashls", "ts_ls", "jdtls"},
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      
+      lspconfig.lua_ls.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.jdtls.setup({})
+    end
   }
-  
+}
